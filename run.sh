aae7d40f3912a4537ca3e3d2bba8d6dee4cdd5b3
kspatel95 <kspatel95@gmail.com>
2020-05-09 21:54:35 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,10 +6,10 @@ pip install -r requirements.txt
 cd ..
 sudo apt-get update
 sudo apt-get install -y ros-kinetic-dbw-mkz-msgs
-cd /home/workspace/Self-Driving-Car/ros
+
+cd ros
 rosdep install --from-paths src --ignore-src --rosdistro=kinetic -y
 
-cd /home/workspace/Self-Driving-Car/ros
 catkin_make
 source devel/setup.sh
 roslaunch launch/styx.launch
