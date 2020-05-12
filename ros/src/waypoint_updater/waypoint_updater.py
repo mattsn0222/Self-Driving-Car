ab6c4b0b9bdb935316993b882ff1a7de652d7eab
mattsn0222 <tomoya.matsumura@gmail.com>
2020-05-12 01:47:19 +0000

    completed simulated traffic light

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -23,7 +23,7 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+LOOKAHEAD_WPS = 50 # Number of waypoints we will publish. You can change this number
 MAX_JERK = 0.5 # m/s2
 MAX_ACCEL = 0.5 # m/s2
 PUBLISH_RATE = 30 # the consumer (waypoint follower) is running at 30 Hz supposedly
