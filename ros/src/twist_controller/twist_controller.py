433aa444f6858f464b06f6c09435de073433bc2c
mattsn0222 <tomoya.matsumura@gmail.com>
2020-05-15 21:59:56 +0000

    update on pid, deceleration algorithm

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -16,7 +16,7 @@ class Controller(object):
         # PID Values
         kp = 0.3
         ki = 0.1
-        kd = 0.
+        kd = 0.1
         mn = 0. # Minimum throttle value
         mx = 0.2 # Maximum throttle value
         self.throttle_controller = PID(kp, ki, kd, mn, mx)
