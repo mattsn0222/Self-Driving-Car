a47ad711ff892189d5e9abe54771a0408b8adb90
esp32wrangler <44122875+esp32wrangler@users.noreply.github.com>
2020-05-16 16:17:28 +0200

    Smarter evaluation algorithm for TL Classifier, upgrade to Fablemagic SSD7 model

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -24,7 +24,7 @@ class RunMode():
     ROSBAG_PLAYBACK = 2
     GENERATE_IMAGES = 4
 
-RUN_MODE = RunMode.NORMAL 
+RUN_MODE = RunMode.NORMAL
 
 def softmax(x):
     e_x = np.exp(x - np.max(x))
@@ -73,6 +73,77 @@ class TLClassifier(object):
 
             self.tf_session = tf.Session(graph=detection_graph)
 
+    def get_crop_classification(self, cv_image,  cropx, cropy, cropwidth, cropheight):
+        orig_image = cv_image
+        if cropwidth < cv_image.shape[1] or cropheight < cv_image.shape[0]:
+            cv_image = cv_image[cropy:cropy+cropheight, cropx:cropx+cropwidth].copy()
+
+        cv_image = cv2.resize(cv_image, (416, 416))
+        cv_image_rgb = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+
+        cv_images = np.reshape(cv_image_rgb, (1, 416, 416, 3))
+        y_pred = self.tf_session.run(self.detection,
+                                     feed_dict={self.input_tensor: cv_images, self.keras_learning: 0})
+
+        y_pred_decoded = decode_detections(y_pred,
+                                           confidence_thresh=0.7,
+                                           iou_threshold=0.45,
+                                           top_k=200,
+                                           normalize_coords=True,
+                                           img_height=cropheight,
+                                           img_width=cropwidth)
+
+        max_confidences = [0] * 3
+        sum_confidences = [0] * 3
+        argm = 3  # default is none
+        if len(y_pred_decoded) > 0:
+            y_pred_list = y_pred_decoded[0].tolist()
+            for cls, conf, xmin, ymin, xmax, ymax in y_pred_list:
+                if conf > max_confidences[int(cls - 1)]:
+                    max_confidences[int(cls - 1)] = conf
+                sum_confidences[int(cls - 1)] += conf
+        if np.max(max_confidences) != 0:
+            highest_confs = np.argwhere(max_confidences == np.amax(max_confidences))
+            argm = highest_confs[0]
+            if len(highest_confs) > 1:
+                max_sum = 0
+                for idx in highest_confs:
+                    if sum_confidences[idx] > max_sum:
+                        max_sum = sum_confidences[idx]
+                        argm = idx
+
+        rospy.logwarn('Prediction %s R:%0.2f Y:%0.2f G:%0.2f' % (
+        inference_map_text_ssd7[argm], max_confidences[1], max_confidences[0], max_confidences[2]))
+
+        dbg_image = None
+        if RUN_MODE & RunMode.GENERATE_IMAGES:
+            dbg_image = orig_image.copy()
+            boxColors = np.array([(0, 255, 255), (0, 0, 255), (0, 255, 0)])
+            white = np.array([255, 255, 255])
+            if len(y_pred_decoded) > 0:
+                y_pred_list = y_pred_decoded[0].tolist()
+                for sss in y_pred_list:
+                    # print sss
+                    cls, conf, xmin, ymin, xmax, ymax = sss
+                    colorMul = (0.3 - (conf - 0.7)) * 3
+                    boxColor = boxColors[int(cls) - 1] * (1 - colorMul) + white * colorMul
+                    cv2.rectangle(dbg_image, (int(xmin+cropx), int(ymin+cropy)),
+                                  (int(xmax+cropx), int(ymax+cropy)), boxColor, 2)
+            cv2.putText(dbg_image,
+                                     'Prediction %s R:%0.2f Y:%0.2f G:%0.2f' % (
+                                     inference_map_text_ssd7[argm], max_confidences[1], max_confidences[0],
+                                     max_confidences[2]),
+                                     (50, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                                     fontScale=1, color=(0, 0, 0), thickness=3)
+            cv2.putText(dbg_image,
+                                     'Prediction %s R:%0.2f Y:%0.2f G:%0.2f' % (
+                                     inference_map_text_ssd7[argm], max_confidences[1], max_confidences[0],
+                                     max_confidences[2]),
+                                     (50, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                                     fontScale=1, color=(255, 255, 255), thickness=2)
+
+        return inference_map_code_ssd7[argm], dbg_image
+
     def get_classification(self, cv_image):
         """Determines the color of the traffic light in the image
         Args:
@@ -83,63 +154,26 @@ class TLClassifier(object):
         if (self.using_ssd7):
             # for the site image the camera is positioned poorly and a large part of the image
             # is the hood. Therefore we crop off the top left corner, which is the relevant part
-            # (as long as the car is going clockwise - otherwise we should crop both the
-            # top left and top right and run a recognition on both, adding the results)
-            orig_image=cv_image
+            # (as long as the car is going clockwise) If we find no traffic light there, we also crop the
+            # top right and run a recognition on that as well)
+            res = TrafficLight.UNKNOWN
+            dbgimage = None
             if self.is_site:
-                cv_image = cv_image[0:416, 0:554].copy()
+                res, dbgimage = self.get_crop_classification(cv_image, 0, 0, 554, 416)
+                if res == TrafficLight.UNKNOWN:
+                    res, dbgimage = self.get_crop_classification(cv_image, cv_image.shape[1]-554, 0, 554, 416)
+            else:
+                res, dbgimage = self.get_crop_classification(cv_image, 0, 0, cv_image.shape[1], cv_image.shape[0])
 
-            # save it at this point, as this is the most useful for later image generation
-            img_height=cv_image.shape[0]
-            img_width=cv_image.shape[1]
-
-            cv_image = cv2.resize(cv_image, (416, 416))
-            cv_image_rgb = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
-
-            cv_images = np.reshape(cv_image_rgb, (1, 416, 416, 3))
-            y_pred = self.tf_session.run(self.detection,
-                                         feed_dict={self.input_tensor: cv_images, self.keras_learning: 0})
-
-
-
-            y_pred_decoded = decode_detections(y_pred,
-                                               confidence_thresh=0.7,
-                                               iou_threshold=0.45,
-                                               top_k=200,
-                                               normalize_coords=True,
-                                               img_height=img_height,
-                                               img_width=img_width)
-
-            votes=[0]*3
-            argm = 3 # default is none
-            if len(y_pred_decoded) > 0:
-                y_pred_list = y_pred_decoded[0].tolist()
-                for cls, conf, xmin, ymin, xmax, ymax in y_pred_list:
-                    votes[int(cls-1)] += 1
-            if np.max(votes) != 0:
-                argm = np.argmax(votes)
-
-            rospy.logwarn('Guessed ' + inference_map_text_ssd7[argm] + " votes(YRG) " + str(votes))
-
-            if RUN_MODE & RunMode.GENERATE_IMAGES:
-                boxColors = [(0, 255, 255), (0, 0, 255), (0, 255, 0)]
-                if len(y_pred_decoded) > 0:
-                   y_pred_list = y_pred_decoded[0].tolist()
-                   for sss in y_pred_list:
-                       # print sss
-                       cls, conf, xmin, ymin, xmax, ymax = sss
-                       boxColor = boxColors[int(cls) - 1]
-                       cv2.rectangle(orig_image, (int(xmin), int(ymin)),
-                                     (int(xmax), int(ymax)), boxColor, 2)
-                orig_image = cv2.putText(orig_image,
-                                           'Guessed ' + inference_map_text_ssd7[argm] + str(votes),
-                                           (50,50), cv2.FONT_HERSHEY_SIMPLEX,
-                                           fontScale=1, color=(255,255,255), thickness=2)
-                #outimg = cv2.cvtColor(orig_image, cv2.COLOR_RGB2BGR)
-                cv2.imwrite("recog_%04d.png" % self.img_count, orig_image)
+            if dbgimage is not None:
+                cv2.imwrite("recog_%04d.png" % self.img_count, dbgimage)
                 self.img_count += 1
 
-            return inference_map_code_ssd7[argm]
+
+            return res
+
+
+
         else:
             #TODO: remove obsolete approach
             cv_image = cv2.resize(image, (224, 224))
